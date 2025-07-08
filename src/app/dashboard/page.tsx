ca4b5461fc152dd22e3d1f56a4883f4caa1cecd1
Anna <annasaltveit@gmail.com>
2025-07-07 22:31:01 -0700

    basic side nav and routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "Dashboard",
+  description: "This is the dashboard page for the ROTAS Map",
+};
+
+
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
